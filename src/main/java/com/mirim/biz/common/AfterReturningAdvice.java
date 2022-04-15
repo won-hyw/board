3b22431d4e0761a55835d224071696b9961e0bcd
won-hyw <w2019@e-mirim.hs.kr>
2022-04-15 09:21:48 +0900

    [220415] AfterReturningAdvice, AfterThrowingAdvice, AroundAdvice JoinPoint를 파라미터로 추가

--- src/main/java/com/mirim/biz/common/AfterReturningAdvice.java
+++ src/main/java/com/mirim/biz/common/AfterReturningAdvice.java
@@ -1,7 +1,18 @@
 package com.mirim.biz.common;
 
+import com.mirim.biz.user.UserVO;
+import org.aspectj.lang.JoinPoint;
+
 public class AfterReturningAdvice {
-    public void afterReturningLog() {
-        System.out.println("[사후처리] 결과값 리턴 후 동작");
+    public void afterReturningLog(JoinPoint jp, Object returnObj) {
+        String method = jp.getSignature().getName();
+
+        if (returnObj instanceof UserVO) {
+            UserVO user = (UserVO) returnObj;
+            if (user.getRole().equals("Admin")) {
+                System.out.println(user.getName() + "로그인(Admin)");
+            }
+        }
+        System.out.println("[사후처리] " + method + "() 메소드 리턴 값 : " + returnObj.toString());
     }
 }
--- src/main/java/com/mirim/biz/common/AroundAdvice.java
+++ src/main/java/com/mirim/biz/common/AroundAdvice.java
@@ -1,12 +1,16 @@
 package com.mirim.biz.common;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.springframework.util.StopWatch;
 
 public class AroundAdvice {
     public Object aroundLog(ProceedingJoinPoint pjp) throws Throwable{
-        System.out.println("[Before] 비즈니스 메소드 수행 전에 처리할 내용");
+        String method = pjp.getSignature().getName();
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start(); // before 대신
         Object returnObj = pjp.proceed();
-        System.out.println("[After] 비즈니스 메소드 수행 후에 처리할 내용");
+        stopWatch.stop(); // after 역할
+        System.out.println(method + "() 메소드 수행에 걸린 시간 : " + stopWatch.getTotalTimeMillis() + "(ms)초");
         return returnObj;
     }
 }
